212da6a7541562861d39999a43357294a8ffc441
Antara055 <antara55.dgp10.1434@gmail.com>
2021-10-29 00:21:38 +0530

    Added: Starting position

--- src/com/bridgelab/SnakeLadder.java
+++ src/com/bridgelab/SnakeLadder.java
@@ -3,5 +3,7 @@ package com.bridgelab;
 public class SnakeLadder {
         public static void main(String[] args) {
             System.out.println("welcome to SNAKE and LADDER Game");
+            int start_pos = 0;
+            System.out.println("Starting position is at :" +start_pos);
         }
 }
