3f386f6805b0a1b7f9a65da1d828fb254bf78783
Antara055 <antara55.dgp10.1434@gmail.com>
2021-10-29 00:24:49 +0530

    Added: Roll the dice

--- src/com/bridgelab/SnakeLadder.java
+++ src/com/bridgelab/SnakeLadder.java
@@ -5,5 +5,19 @@ public class SnakeLadder {
             System.out.println("welcome to SNAKE and LADDER Game");
             int start_pos = 0;
             System.out.println("Starting position is at :" +start_pos);
+            int diceVal = (int) Math.floor((Math.random()*6) + 1);
+            System.out.println("dice value is : " +diceVal);
+            int chk = (int) Math.floor(Math.random()*10 % 3 + 1);
+            switch(chk) {
+                case 1:
+                    start_pos = start_pos + diceVal;
+                    break;
+                case 2:
+                    start_pos = start_pos - diceVal;
+                    break;
+                default:
+                    System.out.println("player is not playing");
+            }
+            System.out.println("player position is now: " +start_pos);
         }
 }
